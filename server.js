b8a6180bb9e23b4ef45bbb321ee8bbfa71cf0a35
Kyle Devine <devinenoise@gmail.com>
2020-02-26 14:29:04 -0800

    user auth

--- server.js
+++ server.js
@@ -26,6 +26,41 @@ app.use(express.json()); // enable reading incoming json data
 app.use(express.urlencoded({ extended: true }));
 
 
+
+// Auth Routes
+const createAuthRoutes = require('./auth/create-auth-routes');
+
+const authRoutes = createAuthRoutes({
+    selectUser(email) {
+        return client.query(`
+            SELECT id, email, hash 
+            FROM users
+            WHERE email = $1;
+        `,
+            [email]
+        ).then(result => result.rows[0]);
+    },
+    insertUser(user, hash) {
+        return client.query(`
+            INSERT into users (email, hash)
+            VALUES ($1, $2)
+            RETURNING id, email;
+        `,
+            [user.email, hash]
+        ).then(result => result.rows[0]);
+    }
+});
+
+// before ensure auth, but after other middleware:
+app.use('/api/auth', authRoutes);
+
+
+// everything that starts with "/api" below here requires an auth token!
+const ensureAuth = require('./auth/ensure-auth');
+app.use('/api', ensureAuth);
+
+
+
 app.get('/api/todos', async (req, res) => {
 
     try {
